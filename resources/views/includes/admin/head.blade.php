b161f81fc025ecbf967a0c2176ddee3d53847012
Janyapron Praserdkul <janyapron2004@gmail.com>
2022-09-01 08:55:55 +0700

    first commit

---
+++ resources/views/includes/admin/head.blade.php
@@ -0,0 +1,39 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Corona Admin</title>
+<!-- plugins:css -->
+<link rel="stylesheet" href=" {{ asset('template/admin/vendors/mdi/css/materialdesignicons.min.css')}}">
+<link rel="stylesheet" href=" {{ asset('template/admin/vendors/css/vendor.bundle.base.css')}}">
+<!-- endinject -->
+<!-- Plugin css for this page -->
+<link rel="stylesheet" href=" {{ asset('template/admin/vendors/jvectormap/jquery-jvectormap.css')}}">
+<link rel="stylesheet" href=" {{ asset('template/admin/vendors/flag-icon-css/css/flag-icon.min.css')}}">
+<link rel="stylesheet" href=" {{ asset('template/admin/vendors/owl-carousel-2/owl.carousel.min.css')}}">
+<link rel="stylesheet" href=" {{ asset('template/admin/vendors/owl-carousel-2/owl.theme.default.min.css')}}">
+<!-- End plugin css for this page -->
+<!-- inject:css -->
+<!-- endinject -->
+<!-- Layout styles -->
+<link rel="stylesheet" href=" {{ asset('template/admin/css/style.css')}}">
+<!-- End layout styles -->
+<link rel="shortcut icon" href=" {{ asset('template/admin/images/favicon.png')}}" />
+
+
+<script src=" {{ asset('template/admin/vendors/js/vendor.bundle.base.js')}}"></script>
+<!-- endinject -->
+<script src=" {{ asset('template/admin/vendors/chart.js/Chart.min.js')}}"></script>
+<script src=" {{ asset('template/admin/vendors/progressbar.js/progressbar.min.js')}}"></script>
+<script src=" {{ asset('template/admin/vendors/jvectormap/jquery-jvectormap.min.js')}}"></script>
+<script src=" {{ asset('template/admin/vendors/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+<script src=" {{ asset('template/admin/vendors/owl-carousel-2/owl.carousel.min.js')}}"></script>
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src=" {{ asset('template/admin/js/off-canvas.js')}}"></script>
+<script src=" {{ asset('template/admin/js/hoverable-collapse.js')}}"></script>
+<script src=" {{ asset('template/admin/js/misc.js')}}"></script>
+<script src=" {{ asset('template/admin/js/settings.js')}}"></script>
+<script src=" {{ asset('template/admin/js/todolist.js')}}"></script>
+<!-- endinject -->
+<!-- Custom js for this page -->
+<script src=" {{ asset('template/admin/assets/js/dashboard.js')}}"></script>
+<!-- End custom js for this page -->
---
+++ resources/views/includes/promote/head.blade.php
@@ -0,0 +1,37 @@
+{{ asset('template/promote')}}
+<link rel="icon" href="img/logo1.jpg">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="{{ asset('template/promote/css/bootstrap.min.css')}}">
+<!-- animate CSS -->
+<link rel="stylesheet" href="{{ asset('template/promote/css/animate.css')}}">
+<!-- owl carousel CSS -->
+<link rel="stylesheet" href="{{ asset('template/promote/css/owl.carousel.min.css')}}">
+<!-- themify CSS -->
+<link rel="stylesheet" href="{{ asset('template/promote/css/themify-icons.css')}}">
+<!-- flaticon CSS -->
+<link rel="stylesheet" href="{{ asset('template/promote/css/flaticon.css')}}">
+<!-- magnific popup CSS -->
+<link rel="stylesheet" href="{{ asset('template/promote/css/magnific-popup.css')}}">
+<!-- nice select CSS -->
+<link rel="stylesheet" href="{{ asset('template/promote/css/nice-select.css')}}">
+<!-- swiper CSS -->
+<link rel="stylesheet" href="{{ asset('template/promote/css/slick.css')}}">
+<!-- style CSS -->
+<link rel="stylesheet" href="{{ asset('template/promote/css/style.css')}}">
+
+<script src="{{ asset('template/promote/js/jquery-1.12.1.min.js')}}"></script>
+<!-- popper js -->
+<script src="{{ asset('template/promote/js/popper.min.js')}}"></script>
+<!-- bootstrap js -->
+<script src="{{ asset('template/promote/js/bootstrap.min.js')}}"></script>
+<!-- owl carousel js -->
+<script src="{{ asset('template/promote/js/owl.carousel.min.js')}}"></script>
+<script src="{{ asset('template/promote/js/jquery.nice-select.min.js')}}"></script>
+<!-- contact js -->
+<script src="{{ asset('template/promote/js/jquery.ajaxchimp.min.js')}}"></script>
+<script src="{{ asset('template/promote/js/jquery.form.js')}}"></script>
+<script src="{{ asset('template/promote/js/jquery.validate.min.js')}}"></script>
+<script src="{{ asset('template/promote/js/mail-script.js')}}"></script>
+<script src="{{ asset('template/promote/js/contact.js')}}"></script>
+<!-- custom js -->
+<script src="{{ asset('template/promote/js/custom.js')}}"></script>
